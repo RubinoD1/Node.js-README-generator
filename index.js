0fc0657f422aca7f91e01c10538fa49647ce7de7
RubinoD1 <Rubinod1@gmail.com>
2022-07-01 00:49:50 -0400

    added process.argv and is working

---
+++ index.js
@@ -0,0 +1,40 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+//const questions = [];
+
+// TODO: Create a function to write README file
+//function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+//function init() {}
+
+// Function call to initialize app
+//init();
+
+//input
+const readmeDataArgs = process.argv.slice(2, process.argv.length);
+console.log(readmeDataArgs);
+
+
+
+
+
+
+
+
+/* module said to remove keeping just in case. ***
+//takes in the input and display output.
+const printReadmeData = readmeDataArr => {
+   for (let i = 0; i < readmeDataArr.length; i++) {
+    console.log(readmeDataArr[i]);
+   }
+};
+
+//console.log('============');
+
+//readmeDataArr.forEach(readmeItem => console.log(readmeItem));
+
+printReadmeData(readmeDataArgs);
+
+*/
