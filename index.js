1d77f3f1e4b36869e862081521c5e1b26e57e722
RubinoD1 <Rubinod1@gmail.com>
2022-07-02 18:51:05 -0400

    added title and description to generate in terminal with hardcoded info

--- index.js
+++ index.js
@@ -16,25 +16,16 @@
 const readmeDataArgs = process.argv.slice(2, process.argv.length);
 console.log(readmeDataArgs);
 
-
-
-
-
-
-
-
-/* module said to remove keeping just in case. ***
-//takes in the input and display output.
-const printReadmeData = readmeDataArr => {
-   for (let i = 0; i < readmeDataArr.length; i++) {
-    console.log(readmeDataArr[i]);
-   }
+//uses backticks ` not '****
+const generateReadme = (titleName, descriptionInfo) => {
+   return `
+      Title: ${titleName}
+      Description: ${descriptionInfo}
+ `; 
 };
+console.log(generateReadme('My README', 'A README file'));
 
-//console.log('============');
 
-//readmeDataArr.forEach(readmeItem => console.log(readmeItem));
 
-printReadmeData(readmeDataArgs);
 
-*/
+
