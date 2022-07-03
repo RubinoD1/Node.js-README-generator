362da898995b376b47f2a5da58e2709323f983e7
RubinoD1 <Rubinod1@gmail.com>
2022-07-02 21:12:35 -0400

    added fs.writefile and base error handler

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ console.log(readmeDataArgs);
 
 
 //uses backticks ` not '****
+//ReadMe template literal
 const generateReadme = (title, description) => {
    return `
       # ${title}
@@ -29,9 +30,18 @@ const generateReadme = (title, description) => {
      ${description}
  `; 
 };
-console.log(title, description);
-console.log(generateReadme(title, description));
 
+/*fs.writefile
+The first argument is the file name that will be created, or the output file.
+The second argument is the data that is being written: the README string template.
+The third argument is the callback function that will handle any errors as we as the success message.
+*/
+fs.writeFile('README.md', generateReadme(title, description), err => {
+   if (err) throw err;
+
+
+   console.log('README created! Check out README.md to see the output!');
+});
 
 
 
