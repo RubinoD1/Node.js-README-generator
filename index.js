0cf4ed67bdd1636453e4bb8fe06b2422463bf888
RubinoD1 <Rubinod1@gmail.com>
2022-07-03 00:13:30 -0400

    added base inquirer questions and two validation checks

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const inquirer = require('inquirer');
 
 
 //pageHTML****
-//const readme = generateReadme(title, description);
+//const readme = generateReadme(title, description, installation, usage, license, tests, github, email);
 //console.log(readmeDataArgs);
 
 
@@ -33,7 +33,7 @@ The first argument is the file name that will be created, or the output file.
 The second argument is the data that is being written: the README string template.
 The third argument is the callback function that will handle any errors as we as the success message.
 
-fs.writeFile('README.md', generateReadme(title, description), err => {
+fs.writeFile('README.md', generateReadme(title, description, installation, usage, license, tests, github, email), err => {
    if (err) throw err;
 
 
@@ -42,16 +42,77 @@ fs.writeFile('README.md', generateReadme(title, description), err => {
 
 */
 
-inquirer
-   .prompt([
+const promptUser = () => {
+   return inquirer.prompt([
       {
          type: 'input',
          name: 'title',
-         message: 'What is the title of your README?'
+         message: 'What is the title of your README? (Required)',
+         //check to make sure user has inputted the required info 
+         validate: nameInput => {
+            if (nameInput) {
+               return true;
+            } else {
+               console.log('Please enter a title for the README');
+            }
+         }
+      },
+      {
+         type: 'input',
+         name: 'description',
+         message: 'Enter a description'
+      },
+      {
+         type: 'input',
+         name: 'installation',
+         message: 'Enter installation instructions'
+      },
+      {
+         type: 'input',
+         name: 'usage',
+         message: 'Enter usage information'
+      },
+      {
+         //type: checkbox 
+         type: 'input',
+         name: 'license',
+         message: 'Enter license information'
+         //choices: ['','',]
+      },
+      {
+         type: 'input',
+         name: 'contributing',
+         message: 'Enter contributing instructions'
+      },
+      {
+         type: 'input',
+         name: 'tests',
+         message: 'Enter tests information'
+      },
+      {
+         type: 'input',
+         name: 'github',
+         message: 'What is your GitHub user name? (Required)',
+         validate: nameInput => {
+            if (nameInput) {
+               return true;
+            } else {
+               console.log('Please enter a GitHub user name');
+            }
+         }
+      },
+      {
+         type: 'input',
+         name: 'email',
+         message: 'What is your email address?'
       }
-   ])
-     .then(answers => console.log(answers));
+   ]);
+};
 
+promptUser()
+.then(answers => console.log(answers))
+.then(promptUser)
+.then(userAnswers => console.log(userAnswers));
 
 
 
--- src/readme-template.js
+++ src/readme-template.js
@@ -1,12 +1,24 @@
 //uses backticks ` not '****
 //ReadMe template literal
-const generateReadme = (title, description) => {
+const generateReadme = (title, description, installation, usage, license, tests, github, email) => {
     return `
        # ${title}
       ## Description 
       ${description}
+      ## Table of contents
+      ## Installation
+      ${installation}
+      ## Usage
+      ${usage}
+      ## License
+      ${license}
+      ## Tests
+      ${tests}
+      ## Questions
+      Github: ${github}
+      Contact me by email: ${email}
   `; 
  };
 
- 
+
  module.exports = generateReadme;
